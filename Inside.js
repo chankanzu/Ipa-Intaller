ab130d905e3d4ab2a65dde0fd8505dc85fb5549f
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2019-11-13 22:05:43 +0700

    Create Inside.js

---
+++ Inside.js
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>items</key>
+	<array>
+		<dict>
+			<key>assets</key>
+			<array>
+				<dict>
+					<key>kind</key>
+					<string>software-package</string>
+					<key>url</key>
+					<string>https://www.dropbox.com/s/zmbzwxrd35mx1lg/INSIDE%201.0.264.ipa?dl=1</string>
+				</dict>
+			</array>
+			<key>metadata</key>
+			<dict>
+				<key>bundle-identifier</key>
+				<string>com.game.inside</string>
+				<key>bundle-version</key>
+				<string>21.03</string>
+				<key>kind</key>
+				<string>software</string>
+				<key>subtitle</key>
+				<string>Inside</string>
+				<key>title</key>
+				<string>Inside</string>
+			</dict>
+		</dict>
+	</array>
+</dict>
+</plist>
