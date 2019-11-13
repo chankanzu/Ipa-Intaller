b1f89838886957d9b57a72d76378668b46f93c47
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2019-11-13 22:17:59 +0700

    Create Deathcell.js

---
+++ Deathcell.js
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>items</key>
+	<array>
+		<dict>
+			<key>assets</key>
+			<array>
+				<dict>
+					<key>kind</key>
+					<string>software-package</string>
+					<key>url</key>
+					<string>https://www.dropbox.com/s/pug9m9jtvl44wt1/Dead%20Cells%201.17.ipa?dl=1</string>
+				</dict>
+			</array>
+			<key>metadata</key>
+			<dict>
+				<key>bundle-identifier</key>
+				<string>com.game.inside</string>
+				<key>bundle-version</key>
+				<string>1.1</string>
+				<key>kind</key>
+				<string>software</string>
+				<key>subtitle</key>
+				<string>Deathcell</string>
+				<key>title</key>
+				<string>Deathcell</string>
+			</dict>
+		</dict>
+	</array>
+</dict>
+</plist>
