24c59367022c0b27684675876065cd0a2dc6a685
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2019-11-13 22:00:40 +0700

    Create Drawingforkids3.52.js

---
+++ Drawingforkids3.52.js
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>items</key>
+	<array>
+		<dict>
+			<key>assets</key>
+			<array>
+				<dict>
+					<key>kind</key>
+					<string>software-package</string>
+					<key>url</key>
+					<string>https://www.dropbox.com/s/oo55q2ds8klobse/Drawing%E2%80%82for%E2%80%82kids%203.52.ipa?dl=1</string>
+				</dict>
+			</array>
+			<key>metadata</key>
+			<dict>
+				<key>bundle-identifier</key>
+				<string>com.ungdung.tomau</string>
+				<key>bundle-version</key>
+				<string>3.52</string>
+				<key>kind</key>
+				<string>software</string>
+				<key>subtitle</key>
+				<string>Tap_to</string>
+				<key>title</key>
+				<string>Tap_to</string>
+			</dict>
+		</dict>
+	</array>
+</dict>
+</plist>
