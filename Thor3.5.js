ffc3a3bbffe0a7c1b935d9f395ceb4f6c136a488
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2019-12-28 11:50:26 +0700

    Create Thor3.5.js

---
+++ Thor3.5.js
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>items</key>
+	<array>
+		<dict>
+			<key>assets</key>
+			<array>
+				<dict>
+					<key>kind</key>
+					<string>software-package</string>
+					<key>url</key>
+					<string> https://www.dropbox.com/s/7ct1mxqiqsi9v5h/Thor%201.3.5.ipa?dl=1</string>
+				</dict>
+			</array>
+			<key>metadata</key>
+			<dict>
+				<key>bundle-identifier</key>
+				<string>com.ungdung.tomau</string>
+				<key>bundle-version</key>
+				<string>3.52</string>
+				<key>kind</key>
+				<string>software</string>
+				<key>subtitle</key>
+				<string>Tap_to</string>
+				<key>title</key>
+				<string>Tap_to</string>
+			</dict>
+		</dict>
+	</array>
+</dict>
+</plist>
